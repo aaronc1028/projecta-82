51a71347fec55c480b3a18a3750f6a55e842e63b
aaronc1028 <81634761+aaronc1028@users.noreply.github.com>
2021-07-08 19:09:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var lastX, lastY, currentX, currentY;
+var mouseEvent = "empty";
+var color = "black";
+var width = 1;
+
+canvas.addEventListener("mousedown", mouseDownFN);
+canvas.addEventListener("mouseup", mouseUpFN);
+canvas.addEventListener("mouseleave", mouseLeaveFN);
+canvas.addEventListener("mousemove", mouseMoveFN);
+
+function mouseDownFN(e) {
+    mouseEvent = "down"
+     color=document.getElementById("textInput").value
+     width=document.getElementById("width").value
+     linewidth=document.getElementById("linewidth").value
+}
+function mouseUpFN(e) {
+    mouseEvent = "up"
+}
+function mouseLeaveFN(e) {
+    mouseEvent = "leave"
+}
+function mouseMoveFN(e) {
+    currentX = e.clientX - canvas.offsetLeft
+    currentY = e.clientY - canvas.offsetTop
+    if (mouseEvent == "down") {
+        ctx.beginPath()
+        ctx.strokeStyle = color
+        ctx.lineWidth = width
+        ctx.moveTo(lastX, lastY)
+        ctx.lineTo(currentX, currentY)
+        ctx.stroke()
+    }
+    lastX=currentX
+    lastY=currentY
+}
+function clearcanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+}
